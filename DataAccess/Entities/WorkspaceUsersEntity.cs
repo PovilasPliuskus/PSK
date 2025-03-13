826acd423cb9772fb934fb62d28c39504c432537
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2025-03-13 19:04:49 +0200

    Add WorkspaceUsersEntity

---
+++ DataAccess/Entities/WorkspaceUsersEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Entities;
+
+public class WorkspaceUsersEntity
+{
+    [Key]
+    public required Guid FkUserId { get; set; }
+
+    [Key]
+    public required Guid FkWorkspaceId { get; set; }
+}
