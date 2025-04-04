8ee7d658bd37a633987407bad1abcd2491abcb2b
Dominykas Černovas <cernovas50@gmail.com>
2025-04-04 08:33:18 +0300

    Redundant imports

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useKeycloak } from '@react-keycloak/web';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Container, Navbar, Nav, Button } from 'react-bootstrap';
