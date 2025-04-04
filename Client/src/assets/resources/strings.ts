2d21693c7718eed6429842bae9927deffd8715c1
Dominykas Černovas <cernovas50@gmail.com>
2025-04-04 11:06:03 +0300

    Workspace data representation

--- Client/src/assets/resources/strings.ts
+++ Client/src/assets/resources/strings.ts
@@ -6,6 +6,14 @@ const ScriptResources = {
     IndexParagraph: 'Navigate to the Demo Form to get started!',
     TotalItems: 'Total Items:',
     ItemsPerPage: 'Items per page:',
+    ErrorFetchingWorkspaces: 'Error fetching workspaces',
+    CreateNew: 'Create New',
+    Workspaces: 'Workspaces',
+    WorkspaceId: 'Workspace ID',
+    WorkspaceName: 'Workspace Name',
+    CreatedBy: 'Created By',
+    CreatedAt: 'Created At',
+    LoadingOrLogin: 'Loading... or please log in to view your data.',
 }
 
 export default ScriptResources;
