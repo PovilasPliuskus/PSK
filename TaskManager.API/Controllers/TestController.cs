1aa6897ab11c7a22854e39a5e6d04b9bfa25b92c
rytiskar <rytis.karalius321@gmail.com>
2025-04-07 11:12:01 +0300

    PSK-9 Re-add Authorize attribute

--- TaskManager.API/Controllers/TestController.cs
+++ TaskManager.API/Controllers/TestController.cs
@@ -14,6 +14,7 @@ public class ProtectedController : ControllerBase
     }
 
     [HttpGet("error")]
+    [Authorize]
     public IActionResult GetError()
     {
         throw new SampleException("This is a simulated error");
