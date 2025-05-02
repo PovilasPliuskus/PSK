438a6fcb7f8e2771a8e917b049674a6bd1233ada
rytiskar <rytis.karalius321@gmail.com>
2025-05-02 12:06:42 +0300

    Add more tests

--- TaskManager.API.Tests/Repositories/WorkspaceRepositoryTests.cs
+++ TaskManager.API.Tests/Repositories/WorkspaceRepositoryTests.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Contracts.Pagination;
 using Model = Contracts.Models;
 using DataAccess.Entities;
 using DataAccess.Profiles;
@@ -24,6 +25,44 @@ public class WorkspaceRepositoryTests
 
         _mapper = config.CreateMapper();
     }
+
+    [TestMethod]
+    public async Task GetAllAsync_ReturnsAllWorkspacesCorrectly()
+    {
+        await TransactionHelper.WrapInTransactionAndRollbackAsync(async context =>
+        {
+            // Arrange
+            Guid id1 = Guid.NewGuid();
+            Guid id2 = Guid.NewGuid();
+            Model.WorkspaceWithoutTasks workspace1 = ModelHelper.GetDefaultWorkspaceWithoutTasksModel(id1);
+            Model.WorkspaceWithoutTasks workspace2 = ModelHelper.GetDefaultWorkspaceWithoutTasksModel(id2);
+
+            WorkspaceEntity workspaceEntity1 = _mapper.Map<WorkspaceEntity>(workspace1);
+            WorkspaceEntity workspaceEntity2 = _mapper.Map<WorkspaceEntity>(workspace2);
+            
+            await context.Workspaces.AddRangeAsync(workspaceEntity1, workspaceEntity2);
+            await context.SaveChangesAsync();
+            
+            WorkspaceRepository repository = new(context, _mapper);
+            
+            // Act
+            PaginatedResult<Model.WorkspaceWithoutTasks> paginatedResult = await repository.GetRangeAsync(1, 5);
+            List<Model.WorkspaceWithoutTasks> workspaces = paginatedResult.Items.ToList();
+
+            // Assert
+            Assert.AreEqual(2, workspaces.Count, $"Expected 2 workspaces but got {workspaces.Count}.");
+            Assert.AreEqual(workspaceEntity1.Id, workspaces[0].Id);
+            Assert.AreEqual(workspaceEntity1.CreatedAt, workspaces[0].CreatedAt);
+            Assert.AreEqual(workspaceEntity1.UpdatedAt, workspaces[0].UpdatedAt);
+            Assert.AreEqual(workspaceEntity1.Name, workspaces[0].Name);
+            Assert.AreEqual(workspaceEntity1.FkCreatedByUserEmail, workspaces[0].CreatedByUserEmail);
+            Assert.AreEqual(workspaceEntity2.Id, workspaces[1].Id);
+            Assert.AreEqual(workspaceEntity2.CreatedAt, workspaces[1].CreatedAt);
+            Assert.AreEqual(workspaceEntity2.UpdatedAt, workspaces[1].UpdatedAt);
+            Assert.AreEqual(workspaceEntity2.Name, workspaces[1].Name);
+            Assert.AreEqual(workspaceEntity2.FkCreatedByUserEmail, workspaces[1].CreatedByUserEmail);
+        });
+    }
     
     [TestMethod]
     public async Task AddAsync_GivenValidWorkspace_AddsToDatabase()
@@ -45,7 +84,7 @@ public class WorkspaceRepositoryTests
     }
     
     [TestMethod]
-    public async Task GetAsync_GivenExistingWorkspace_ReturnsItCorrectly()
+    public async Task GetSingleAsync_GivenExistingWorkspace_ReturnsItCorrectly()
     {
         await TransactionHelper.WrapInTransactionAndRollbackAsync(async context =>
         {
